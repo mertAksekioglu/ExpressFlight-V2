f0ecce7a9213813753c8fde68f6e5668dea5a07f
Mert <mert.aksekioglu@gmail.com>
2023-09-01 14:03:46 +0300

    AppConfig simplified, Integrations are now done automatically

--- src/main/java/com/expressflight/ExpressFlight/config/AppConfig.java
+++ src/main/java/com/expressflight/ExpressFlight/config/AppConfig.java
@@ -4,6 +4,7 @@ package com.expressflight.ExpressFlight.config;
 import com.expressflight.ExpressFlight.deserializer.LocalDateDeserializer;
 import com.expressflight.ExpressFlight.deserializer.LocalDateTimeDeserializer;
 import com.expressflight.ExpressFlight.domain.*;
+import com.expressflight.ExpressFlight.integration.IIntegration;
 import com.expressflight.ExpressFlight.microservice.SunExpressFlightIntegrationService;
 import com.expressflight.ExpressFlight.microservice.TicketBooker;
 import com.expressflight.ExpressFlight.repository.*;
@@ -38,49 +39,10 @@ public class AppConfig {
 
     private final DateTimeFormatter HH_mm = DateTimeFormatter.ofPattern("HH:mm");
 
-    private final String resource_path = "src/main/resources/data/";
-
-    private IPlaneRepository planeRepository;
-
-    private IAirportRepository airportRepository;
-
-    private ICoordinateRepository coordinateRepository;
-
-    private IFlightRepository flightRepository;
-
-    private IConnectedFlightRepository connectedFlightRepository;
-
-    private ISeatConfigurationRepository seatConfigurationRepository;
-
-    private IPassengerRepository passengerRepository;
-
-    private IMemberRepository memberRepository;
-
-    private IRoleRepository roleRepository;
-
-    private SeatMapFactory seatMapFactory;
-
-    private JPARepoPopulator populator;
-
     private IDatabasePopulator databasePopulator;
 
-    public AppConfig(JPARepoPopulator populator, IPlaneRepository planeRepository, IAirportRepository airportRepository,
-                     ICoordinateRepository coordinateRepository, IFlightRepository flightRepository,
-                     IConnectedFlightRepository connectedFlightRepository, ISeatConfigurationRepository seatConfigurationRepository,
-                     IPassengerRepository passengerRepository, SeatMapFactory seatMapFactory,
-                     IMemberRepository memberRepository, IRoleRepository roleRepository, IDatabasePopulator databasePopulator) {
 
-        this.populator = populator;
-        this.planeRepository = planeRepository;
-        this.airportRepository = airportRepository;
-        this.coordinateRepository = coordinateRepository;
-        this.flightRepository = flightRepository;
-        this.connectedFlightRepository = connectedFlightRepository;
-        this.seatConfigurationRepository = seatConfigurationRepository;
-        this.passengerRepository = passengerRepository;
-        this.seatMapFactory = seatMapFactory;
-        this.memberRepository = memberRepository;
-        this.roleRepository = roleRepository;
+    public AppConfig(IDatabasePopulator databasePopulator) {
         this.databasePopulator = databasePopulator;
     }
 
@@ -100,52 +62,16 @@ public class AppConfig {
         return new ModelMapper();
     }
 
-  /*  @Bean
-    public JPARepoPopulator jpaRepoPopulator() {
-        var gson = gson();
-        return new JPARepoPopulator(gson);
-    }*/
-
     @Bean
     public RestTemplate restTemplate() {return new RestTemplate();}
 
     @Bean
-    CommandLineRunner commandLineRunner(IPlaneRepository planeRepo,
-                                        IAirportRepository airportRepo,
-                                        IFlightRepository flightRepo,
-                                        IConnectedFlightRepository connectedFlightRepo,
-                                        ISeatConfigurationRepository seatConfigurationRepo,
-                                        IPassengerRepository passengerRepo,
-                                        SunExpressFlightIntegrationService sunExpressFlightIntegrationService,
-                                        TicketBooker ticketBooker,
-                                        Gson gson) {
-
+    CommandLineRunner commandLineRunner(SunExpressFlightIntegrationService sunExpressFlightIntegrationService) {
         return args -> {
-            //populateAllRepositories(gson);
+            var gson = gson();
             databasePopulator.populateDatabase(gson);
+            doAllFlightIntegrations(sunExpressFlightIntegrationService);
         };
-    }
-
-    public List<SeatConfiguration> configureAllFlightSeatConfigs() {
-        List<SeatConfiguration> unconfiguredSeatConfigs = seatConfigurationRepository.findByIsConfigured(false);
-        for (SeatConfiguration seatConfig : unconfiguredSeatConfigs)
-        {
-            seatConfig.setSeatMap(seatMapFactory.createSeatMap(seatConfig.getConfigName()).mapSeats());
-            seatConfig.setIsConfigured(true);
-        }
-        return unconfiguredSeatConfigs;
-    }
-
-    public void populateAllRepositories(Gson gson) {
-        populator.populateRepo(planeRepository, resource_path + "plane_data.json", Plane[].class,gson);
-        populator.populateRepo(airportRepository, resource_path + "airport_data.json", Airport[].class,gson);
-        populator.populateRepo(flightRepository, resource_path + "flight_data.json", Flight[].class,gson);
-        populator.populateRepo(connectedFlightRepository, resource_path + "connected_flight_data.json", ConnectedFlight[].class,gson);
-        populator.populateRepo(seatConfigurationRepository, resource_path + "seat_configuration_data.json", SeatConfiguration[].class,gson);
-        populator.populateRepo(passengerRepository, resource_path + "passenger_data.json", Passenger[].class,gson);
-        populator.populateRepo(coordinateRepository, resource_path + "coordinate_data.json", Coordinate[].class,gson);
-        populator.populateRepo(memberRepository, resource_path + "member_data.json", Member[].class,gson);
-        populator.populateRepo(roleRepository, resource_path + "role_data.json", Role[].class,gson);
     }
 
     public void doAllFlightIntegrations(SunExpressFlightIntegrationService sunExpressFlightIntegrationService) {
